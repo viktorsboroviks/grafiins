c887169927f050fb5b5f547569db06ff26c8e8d0
Viktors Boroviks <vboroviks@inbox.lv>
2024-07-12 00:19:19 +0300

    More graph operations added

--- examples/graph.cpp
+++ examples/graph.cpp
@@ -5,5 +5,23 @@ int main()
     vgraph::Graph<vgraph::Vertex, vgraph::Edge> g;
     assert(g.get_vertices().size() == 0);
     assert(g.get_edges().size() == 0);
+
+    assert(g.add_vertex(vgraph::Vertex()) == 0);
+    assert(g.add_vertex() == 1);
+    assert(g.get_n_vertices() == 2);
+    assert(g.get_vertices().size() == 2);
+
+    g.remove_vertex(0);
+    assert(g.get_n_vertices() == 1);
+    assert(g.get_vertices().size() == 1);
+
+    assert(g.add_vertex() == 0);
+    assert(g.get_n_vertices() == 2);
+    assert(g.get_vertices().size() == 2);
+
+    assert(g.add_edge(0, 1) == 0);
+    assert(g.get_n_edges() == 1);
+    assert(g.get_edges().size() == 1);
+
     return 0;
 }
--- include/vgraph.hpp
+++ include/vgraph.hpp
@@ -11,8 +11,31 @@ private:
     std::set<size_t> _out_edges_i;
 
 public:
-    size_t i;
     bool allocated = false;
+
+    void connect_in_edge(size_t i)
+    {
+        assert(!_in_edges_i.contains(i));
+        _in_edges_i.insert(i);
+    }
+
+    void connect_out_edge(size_t i)
+    {
+        assert(!_out_edges_i.contains(i));
+        _out_edges_i.insert(i);
+    }
+
+    void disconnect_in_edge(size_t i)
+    {
+        assert(_in_edges_i.contains(i));
+        _in_edges_i.erase(i);
+    }
+
+    void disconnect_out_edge(size_t i)
+    {
+        assert(_out_edges_i.contains(i));
+        _out_edges_i.erase(i);
+    }
 
     const std::set<size_t> get_in_edges_i()
     {
@@ -43,8 +66,13 @@ private:
     size_t _dst_vertex_i;
 
 public:
-    size_t i;
     bool allocated = false;
+
+    Edge(size_t in_src_i, size_t in_dst_i) :
+        _src_vertex_i(in_src_i),
+        _dst_vertex_i(in_dst_i)
+    {
+    }
 
     size_t get_src_vertex_i()
     {
@@ -62,40 +90,141 @@ class Graph {
 private:
     std::vector<TVertex> _vertices;
     std::vector<TEdge> _edges;
-    std::queue<size_t> _not_allocated_vertices_i;
-    std::queue<size_t> _not_allocated_edges_i;
+    std::queue<size_t> _unallocated_vertices_i;
+    std::queue<size_t> _unallocated_edges_i;
 
 public:
+    size_t get_n_vertices()
+    {
+        assert(_unallocated_vertices_i.size() <= _vertices.size());
+        return _vertices.size() - _unallocated_vertices_i.size();
+    }
+
+    size_t get_n_edges()
+    {
+        assert(_unallocated_edges_i.size() <= _edges.size());
+        return _edges.size() - _unallocated_edges_i.size();
+    }
+
     const std::vector<TVertex> get_vertices()
     {
-        return _vertices;
+        std::vector<TVertex> vertices;
+        for (auto& v : _vertices) {
+            if (v.allocated) {
+                vertices.push_back(v);
+            }
+        }
+        return vertices;
     }
 
     const std::vector<TEdge> get_edges()
     {
-        return _edges;
+        std::vector<TEdge> edges;
+        for (auto& e : _edges) {
+            if (e.allocated) {
+                edges.push_back(e);
+            }
+        }
+        return edges;
     }
 
-    size_t add_vertex(TVertex)
+    size_t add_vertex(TVertex v)
     {
-        // TODO: add
-        return 0;
+        assert(_unallocated_vertices_i.size() <= _vertices.size());
+
+        v.allocated = true;
+
+        // first try to occupy unallocated indeces
+        if (!_unallocated_vertices_i.empty()) {
+            size_t i = _unallocated_vertices_i.front();
+            _vertices[i] = v;
+            _unallocated_vertices_i.pop();
+            return i;
+        }
+
+        // if no unallocated indeces available, extend storage
+        _vertices.push_back(v);
+        return _vertices.size() - 1;
     }
 
-    size_t add_edge(TVertex src, TVertex dst)
+    size_t add_vertex()
     {
-        // TODO: add
-        return 0;
+        TVertex v{};
+        return add_vertex(v);
     }
 
     void remove_vertex(size_t i)
     {
-        // TODO: add
+        assert(i < _vertices.size());
+        assert(_vertices[i].allocated);
+        // remove connected edges
+        for (size_t i : _vertices[i].get_in_edges_i()) {
+            remove_edge(i);
+        }
+        for (size_t i : _vertices[i].get_out_edges_i()) {
+            remove_edge(i);
+        }
+        // remove vertex
+        _vertices[i].allocated = false;
+        _unallocated_vertices_i.push(i);
+        assert(_unallocated_vertices_i.size() <= _vertices.size());
+    }
+
+    size_t add_edge(TEdge e, size_t src_i, size_t dst_i)
+    {
+        // check input
+        assert(src_i < _vertices.size());
+        assert(dst_i < _vertices.size());
+        assert(_vertices[src_i].allocated);
+        assert(_vertices[dst_i].allocated);
+
+        // allocate edge
+        assert(_unallocated_edges_i.size() <= _edges.size());
+        e.allocated = true;
+        size_t i;
+
+        // first try to occupy unallocated indeces
+        if (!_unallocated_edges_i.empty()) {
+            i = _unallocated_edges_i.front();
+            _edges[i] = e;
+            _unallocated_edges_i.pop();
+        }
+        // if no unallocated indeces available, extend storage
+        else {
+            _edges.push_back(e);
+            i = _edges.size() - 1;
+        }
+
+        // update connected vertices
+        _vertices[src_i].connect_out_edge(i);
+        _vertices[dst_i].connect_in_edge(i);
+
+        return i;
+    }
+
+    size_t add_edge(size_t src_i, size_t dst_i)
+    {
+        TEdge e(src_i, dst_i);
+        return add_edge(e, src_i, dst_i);
     }
 
     void remove_edge(size_t i)
     {
-        // TODO: add
+        assert(i < _edges.size());
+        assert(_edges[i].allocated);
+        // disconnect vertices
+        const size_t src_i = _edges[i].get_src_vertex_i();
+        const size_t dst_i = _edges[i].get_dst_vertex_i();
+        assert(src_i < _vertices.size());
+        assert(_vertices[i].allocated);
+        assert(dst_i < _vertices.size());
+        assert(_vertices[i].allocated);
+        _vertices[src_i].disconnect_in_edge(i);
+        _vertices[dst_i].disconnect_in_edge(i);
+        // remove edge
+        _edges[i].allocated = false;
+        _unallocated_edges_i.push(i);
+        assert(_unallocated_edges_i.size() <= _edges.size());
     }
 };
 
